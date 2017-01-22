a977bca0a17b787b1ca731196c8df9550de84827
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-01-21 17:24:35 -0800

    Fixed Glaxx not acting like glass

--- src/main/java/tehnut/xtones/block/BlockXtone.java
+++ src/main/java/tehnut/xtones/block/BlockXtone.java
@@ -1,12 +1,21 @@
 package tehnut.xtones.block;
 
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import tehnut.xtones.Xtones;
 
+import javax.annotation.Nullable;
 import java.util.Locale;
 
 public class BlockXtone extends BlockEnum<BlockXtone.XtoneType> {
@@ -50,6 +59,42 @@ public class BlockXtone extends BlockEnum<BlockXtone.XtoneType> {
 
     @Override
     public boolean isFullBlock(IBlockState state) {
+        return !seeThrough;
+    }
+
+    @Override
+    public int getLightOpacity(IBlockState state) {
+        return seeThrough ? 0 : super.getLightOpacity(state);
+    }
+
+    @Override
+    public Material getMaterial(IBlockState state) {
+        return seeThrough ? Material.GLASS : super.getMaterial(state);
+    }
+
+    @Override
+    public SoundType getSoundType(IBlockState state, World world, BlockPos pos, @Nullable Entity entity) {
+        return seeThrough ? SoundType.GLASS : super.getSoundType(state, world, pos, entity);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        if (!seeThrough)
+            return super.shouldSideBeRendered(state, world, pos, side);
+
+        IBlockState neighbor = world.getBlockState(pos.offset(side));
+        if (neighbor.getBlock() != this)
+            return true;
+
+        if (neighbor.getValue(getProperty()) != state.getValue(getProperty()))
+            return true;
+
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
         return !seeThrough;
     }
 
