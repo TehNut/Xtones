ed5b0abdf1a5b06ca8e465b406a27bf4615f0207
InsomniaKitten <admin@designdevils.co.uk>
2017-07-26 12:15:31 +0100

    Remove unnecessary overrides in CompatWaila

--- src/main/java/tehnut/xtones/compat/CompatWaila.java
+++ src/main/java/tehnut/xtones/compat/CompatWaila.java
@@ -1,13 +1,8 @@
 package tehnut.xtones.compat;
 
 import mcp.mobius.waila.api.*;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.translation.I18n;
-import net.minecraft.world.World;
 import tehnut.xtones.block.BlockXtone;
 
 import javax.annotation.Nonnull;
@@ -23,29 +18,9 @@ public class CompatWaila implements IWailaPlugin {
 
     public static class DataProviderXtones implements IWailaDataProvider {
         @Override @Nonnull
-        public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
-            return accessor.getStack();
-        }
-
-        @Override @Nonnull
         public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
             currenttip.add(I18n.translateToLocalFormatted("tooltip.xtones.type", itemStack.getItemDamage() + 1));
             return currenttip;
-        }
-
-        @Override @Nonnull
-        public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-            return currenttip;
-        }
-
-        @Override @Nonnull
-        public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-            return currenttip;
-        }
-
-        @Override @Nonnull
-        public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
-            return tag;
         }
     }
 }
