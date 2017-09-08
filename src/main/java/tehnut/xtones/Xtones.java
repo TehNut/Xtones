ce2fa4ff08d272be2bb3594ccf3e03577889cd94
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-09-07 22:12:19 -0700

    Fix recipe errors

--- src/main/java/tehnut/xtones/Xtones.java
+++ src/main/java/tehnut/xtones/Xtones.java
@@ -7,6 +7,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import tehnut.xtones.compat.CompatChisel;
 import tehnut.xtones.network.MessageCycleXtone;
 
@@ -17,6 +19,7 @@ public class Xtones {
 
     public static final String ID = "xtones";
     public static final String NAME = "Xtones";
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final SimpleNetworkWrapper NETWORK_WRAPPER = new SimpleNetworkWrapper(ID);
 
     public static final CreativeTabs TAB = new CreativeTabs(ID) {
@@ -37,5 +40,7 @@ public class Xtones {
 
         if (Loader.isModLoaded("chisel"))
             CompatChisel.sendIMC();
+
+        LOGGER.info("Xtones has loaded with {} unique blocks. If you are struggling to keep within 4096 IDs, you may want to disable some of these.", RegistrarXtones.BLOCKS.size() + 2); // +2 because lamp and base
     }
 }
