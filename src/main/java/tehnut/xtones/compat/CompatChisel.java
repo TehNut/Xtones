669d4c53c7630b66cdfe2c1a10a94a1e864355bd
Nicholas Ignoffo <nick@tehnut.info>
2017-11-20 16:21:28 -0800

    Update Chisel IMC to new API (#11)

--- src/main/java/tehnut/xtones/compat/CompatChisel.java
+++ src/main/java/tehnut/xtones/compat/CompatChisel.java
@@ -1,5 +1,6 @@
 package tehnut.xtones.compat;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import tehnut.xtones.ConfigHandler;
 import tehnut.xtones.RegistrarXtones;
@@ -12,14 +13,23 @@ public class CompatChisel {
     public static void sendIMC() {
         switch (ConfigHandler.chiselMode) {
             case 0: {
-                for (BlockXtone xtone : RegistrarXtones.BLOCKS)
-                    FMLInterModComms.sendMessage("chisel", "variation:add", Xtones.ID + "|" + xtone.getRegistryName() + "|0");
+                for (BlockXtone xtone : RegistrarXtones.BLOCKS) {
+                    NBTTagCompound tag = new NBTTagCompound();
+                    tag.setString("group", Xtones.ID);
+                    tag.setString("block", xtone.getRegistryName().toString());
+                    FMLInterModComms.sendMessage("chisel", "add_variation", tag);
+                }
                 break;
             }
             case 1: {
                 for (BlockXtone xtone : RegistrarXtones.BLOCKS)
-                    for (int i = 0; i < BlockXtone.XtoneType.values().length; i++)
-                        FMLInterModComms.sendMessage("chisel", "variation:add", xtone.getName() + "|" + xtone.getRegistryName() + "|" + i);
+                    for (int i = 0; i < BlockXtone.XtoneType.values().length; i++) {
+                        NBTTagCompound tag = new NBTTagCompound();
+                        tag.setString("group", xtone.getName());
+                        tag.setString("block", xtone.getRegistryName().toString());
+                        tag.setInteger("meta", i);
+                        FMLInterModComms.sendMessage("chisel", "add_variation", tag);
+                    }
                 break;
             }
         }
