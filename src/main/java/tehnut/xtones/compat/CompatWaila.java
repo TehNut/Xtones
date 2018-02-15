5697c1c8d79078a1a8085e105a5884dc5b045750
Nicholas Ignoffo <nick@tehnut.info>
2018-02-14 17:47:08 -0800

    Move Hwyla integration into a lambda

--- src/main/java/tehnut/xtones/compat/CompatWaila.java
+++ src/main/java/tehnut/xtones/compat/CompatWaila.java
@@ -1,8 +1,8 @@
 package tehnut.xtones.compat;
 
 import mcp.mobius.waila.api.*;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.translation.I18n;
 import tehnut.xtones.block.BlockXtone;
 
 import javax.annotation.Nonnull;
@@ -13,15 +13,13 @@ public class CompatWaila implements IWailaPlugin {
 
     @Override
     public void register(IWailaRegistrar registrar) {
-        registrar.registerHeadProvider(new DataProviderXtones(), BlockXtone.class);
-    }
-
-    public static class DataProviderXtones implements IWailaDataProvider {
-        @Override
-        @Nonnull
-        public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-            currenttip.add(I18n.translateToLocalFormatted("tooltip.xtones.type", itemStack.getItemDamage() + 1));
-            return currenttip;
-        }
+        registrar.registerHeadProvider(new IWailaDataProvider() {
+            @Nonnull
+            @Override
+            public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+                currenttip.add(I18n.format("tooltip.xtones.type", itemStack.getItemDamage() + 1));
+                return currenttip;
+            }
+        }, BlockXtone.class);
     }
 }
