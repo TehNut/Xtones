cd0afadf0c43506095328b10131cbc9a3c067367
Chloe Dawn <chloe@sapphic.dev>
2020-05-15 18:16:37 +0100

    Replace cycling warning with server log warning  - This will only be reached if there is a de-sync between client and server, warning only on the server is enough in this case

--- src/main/java/info/tehnut/xtones/network/XtoneCycleHandler.java
+++ src/main/java/info/tehnut/xtones/network/XtoneCycleHandler.java
@@ -1,30 +1,31 @@
 package info.tehnut.xtones.network;
 
+import com.mojang.authlib.GameProfile;
+import info.tehnut.xtones.Tone;
+import info.tehnut.xtones.config.XtonesConfig;
+import info.tehnut.xtones.item.XtoneBlockItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import info.tehnut.xtones.Tone;
-import info.tehnut.xtones.config.XtonesConfig;
-import info.tehnut.xtones.item.XtoneBlockItem;
 
 final class XtoneCycleHandler implements IMessageHandler<XtoneCycleMessage, IMessage> {
     static final XtoneCycleHandler INSTANCE = new XtoneCycleHandler();
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     private XtoneCycleHandler() {
     }
 
     private static void tryCycle(final EntityPlayer player, final XtoneCycleMessage cycle) {
         if (XtonesConfig.disableScrollCycling) {
-            final ITextComponent msg = new TextComponentTranslation("chat.xtones.scroll.disable");
-            msg.getStyle().setColor(TextFormatting.RED);
-            player.sendStatusMessage(msg, true);
+            final GameProfile profile = player.getGameProfile();
+            LOGGER.warn("{} ({}) tried to cycle when cycling is disabled", profile.getName(), profile.getId());
             return;
         }
         final ItemStack held = player.getHeldItem(cycle.getHand());
