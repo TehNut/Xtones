18c81d104222127490e855522e10ff9946b5d33f
Chloe Dawn <chloe@sapphic.dev>
2020-05-16 18:27:54 +0100

    Fix config sync handling not using client task scheduler

--- src/main/java/info/tehnut/xtones/network/ConfigSyncHandler.java
+++ src/main/java/info/tehnut/xtones/network/ConfigSyncHandler.java
@@ -1,11 +1,11 @@
 package info.tehnut.xtones.network;
 
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import info.tehnut.xtones.client.XtonesClient;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import info.tehnut.xtones.client.XtonesClient;
 
 final class ConfigSyncHandler implements IMessageHandler<ConfigSyncMessage, IMessage> {
     static final ConfigSyncHandler INSTANCE = new ConfigSyncHandler();
@@ -20,7 +20,7 @@ final class ConfigSyncHandler implements IMessageHandler<ConfigSyncMessage, IMes
     @Override
     @Nullable
     public IMessage onMessage(final ConfigSyncMessage config, final MessageContext context) {
-        FMLCommonHandler.instance().getMinecraftServerInstance().addScheduledTask(() -> readConfig(config));
+        Minecraft.getMinecraft().addScheduledTask(() -> readConfig(config));
         return null;
     }
 }
