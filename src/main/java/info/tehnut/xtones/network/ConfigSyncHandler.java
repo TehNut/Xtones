c7c7b40b8cb391baa07080ce9c23ce6db1bcaf80
Chloe Dawn <chloe@sapphic.dev>
2020-05-16 18:31:28 +0100

    Use FMLClientHandler for consistency's sake, and add missing SideOnly annotation

--- src/main/java/info/tehnut/xtones/network/ConfigSyncHandler.java
+++ src/main/java/info/tehnut/xtones/network/ConfigSyncHandler.java
@@ -1,10 +1,12 @@
 package info.tehnut.xtones.network;
 
 import info.tehnut.xtones.client.XtonesClient;
-import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 final class ConfigSyncHandler implements IMessageHandler<ConfigSyncMessage, IMessage> {
@@ -19,8 +21,9 @@ final class ConfigSyncHandler implements IMessageHandler<ConfigSyncMessage, IMes
 
     @Override
     @Nullable
+    @SideOnly(Side.CLIENT)
     public IMessage onMessage(final ConfigSyncMessage config, final MessageContext context) {
-        Minecraft.getMinecraft().addScheduledTask(() -> readConfig(config));
+        FMLClientHandler.instance().getClient().addScheduledTask(() -> readConfig(config));
         return null;
     }
 }
